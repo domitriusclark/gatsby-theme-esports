f546318aa51dcb7016b928b0534be7b37db0233d
DomitriusAnthony <domitriusaclark@gmail.com>
2019-08-24 03:36:22 -0400

    Plugin options with colors now pass either user themes or the internal theme.

--- demo/gatsby-config.js
+++ demo/gatsby-config.js
@@ -3,7 +3,12 @@ module.exports = {
     {
       resolve: "gatsby-theme-esports",
       options: {
-        navLinks: ["About", "Team"]
+        navLinks: ["About", "Team"],
+        colors: {
+          background: "#000000",
+          primaryColor: "#FFFFFF",
+          secondaryColor: "#D21823"
+        }
       }
     },
     "gatsby-plugin-transition-link"
